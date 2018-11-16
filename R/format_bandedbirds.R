48634649a0877b61d1e3966fb84e13ed18b456fc
ajmacdonald3 <ajmacdonald3@gmail.com>
2018-11-16 11:56:40 -0500

    added roxygen2 code and code annotations

--- R/format_bandedbirds.R
+++ R/format_bandedbirds.R
@@ -6,39 +6,39 @@
 #' @return Dataframe with columns for ResightDate and FlagID (flag colour and
 #'         code).
 #' @examples
-#' format_bandedbirds(x, sp = "REKN", cert = TRUE)
+#' format_bandedbirds(bandedbirds, sp = "REKN", cert = TRUE)
 
 format_bandedbirds <- function(x, sp = NA, cert = TRUE) {
   if (cert == TRUE) {
 
   # manipulate the BandedBirds data into a format we can work with
   resight_data <- x %>%
-    filter(Species %in% sp) %>%
+    filter(Species %in% sp) %>% # keeps only species of interest
     filter(ResightCertainty == "C") %>% # if resight certainty was consistently recorded!
-    select(ResightDate, FlagColor, FlagCode) %>%
-    fill(ResightDate) %>%
-    mutate(FlagID = paste(FlagColor, FlagCode, sep = "(")) %>%
-    select(ResightDate, FlagID)
+    select(ResightDate, FlagColor, FlagCode) %>% # keeps only these columns
+    fill(ResightDate) %>% # fills in date for every observation
+    mutate(FlagID = paste(FlagColor, FlagCode, sep = "(")) %>% # creates new column that combines flag colour and code
+    select(ResightDate, FlagID) # keeps only these columns
 
-  resight_data$FlagID <- paste0(resight_data$FlagID, ")")
+  resight_data$FlagID <- paste0(resight_data$FlagID, ")") # flag colour and code in PASP format
 
-  assign("resight_data", resight_data, envir = globalenv())
+  assign("resight_data", resight_data, envir = globalenv()) # sends dataframe back to R environment
   }
   else {
 
     # manipulate the BandedBirds data into a format we can work with
     resight_data <- x %>%
-      filter(Species %in% sp) %>%
+      filter(Species %in% sp) %>% # keeps only species of interest
       filter(is.na(ResightCertainty) | ResightCertainty > 94) %>% # if resight certainly wasn't consistently
                                                                   # recorded :(
       filter(!str_detect(FlagCode, "Q")) %>% # if resight certainly wasn't consistently recorded :(
-      select(ResightDate, FlagColor, FlagCode) %>%
-      fill(ResightDate) %>%
-      mutate(FlagID = paste(FlagColor, FlagCode, sep = "(")) %>%
-      select(ResightDate, FlagID)
+      select(ResightDate, FlagColor, FlagCode) %>% # keeps only these columns
+      fill(ResightDate) %>% # fills in date for every observation
+      mutate(FlagID = paste(FlagColor, FlagCode, sep = "(")) %>% # creates new column that combines flag colour and code
+      select(ResightDate, FlagID) # keeps only these columns
 
-    resight_data$FlagID <- paste0(resight_data$FlagID, ")")
+    resight_data$FlagID <- paste0(resight_data$FlagID, ")") # flag colour and code in PASP format
 
-    assign("resight_data", resight_data, envir = globalenv())
+    assign("resight_data", resight_data, envir = globalenv()) # sends dataframe back to R environment
     }
 }
